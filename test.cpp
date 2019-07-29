ea9e14f9cd37c1f21a206482a8193bca9fe67c99
hube12 <h66R10@gmail.com>
2019-07-29 13:52:37 +0200

    Delete test.cpp

--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "httplib.hpp"
-int main()
-{
-    system("curl -X POST https://api.telegram.org/bot830618853:AAGbhpAtk0RJgQ97KmCTQpxeyB6vveD2OJU/sendMessage "
-           "-d chat_id=616875529 -d text=\"test\"");
-}
